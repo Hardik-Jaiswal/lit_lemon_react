12fb9f870acc002c69d4a2d5a5f5256c885cd35e
Hardik Jaiswal <Hardik.Jaiswal@arup.com>
2023-10-03 10:15:22 +0530

    create little lemon app

--- lit_lemon_react_app/src/App.js
+++ lit_lemon_react_app/src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
+import React from "react";
+import Nav from "./Component/Nav";
+import ReserveTable from "./Component/BookTable";
+import Home from "./Component/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */
-      Homepage}
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <div className="App">
+          <Nav />
+          <Routes>
+            <Route path="/" index element={<Home />} />
+            <Route path="/booktable" element={<ReserveTable />} />
+          </Routes>
+        </div>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
